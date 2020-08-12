3efc21febbe7bd0e4777a99cf0c2772da936991c
Francisco Sedano <fran@fransedano.net>
2020-08-12 23:20:51 +0200

    Add istio

--- backend/backend.py
+++ backend/backend.py
@@ -2,10 +2,12 @@ import time
 import logging
 import socket
 
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 import requests
+import random
 app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/healthz')
 def healthz():
@@ -19,7 +21,15 @@ def root():
 
 @app.route('/data')
 def data():
-    return {'data':socket.gethostname()}
+    x = random.randint(0,10)
+    logging.info(request.headers)
+    if x < 5:
+        time.sleep(x)
+    if x < 2:
+        logging.error("FAILING!!!!")
+        return {'error':'fail'},500
+    else:
+        return {'data':socket.gethostname(), 'delay':x}
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host="0.0.0.0")
